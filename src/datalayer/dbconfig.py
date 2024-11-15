daa78a6b0ccc73921e5f85cefcff0e270f9c0b6f
jefersonprimer <jefersonpriimer@gmail.com>
2024-11-15 07:23:04 -0300

    feat: dbconfig

---
+++ src/datalayer/dbconfig.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI 
+from tortoise.contrib.fastapi import register_tortoise
+
+async def configure_db(app: FastAPI):
+    register_tortoise(
+        app=app,
+        config={
+            'connections': {
+                #'default': 'postgres://postgres:qwerty@localhost:5432/events'
+                'default': 'sqlite://db.sqlite3'
+            },
+            'apps': {
+                'models': {
+                    'models': [],
+                    'default_connection': 'default',
+                }
+            }
+
+        }
+    )
